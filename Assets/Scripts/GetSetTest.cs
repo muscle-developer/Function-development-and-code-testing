0befaf7968d4def05d5042634a9378cf2c6b4575
Kimsinyoung <sinyoung5683@fireshrike.com>
2024-03-27 20:27:37 +0900

    테스트 푸시

--- Assets/Scripts/GetSetTest.cs
+++ Assets/Scripts/GetSetTest.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class GetSetTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // getter 와 setter에 관하여
 }
