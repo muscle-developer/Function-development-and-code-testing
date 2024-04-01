16dc37fe50acf41e0c0acf7e775515f306a5bc06
Kimsinyoung <sinyoung5683@fireshrike.com>
2024-04-01 20:14:47 +0900

    자료구조 - Generic Collection - Dictionary에 관한 코드 추가

--- Assets/Scripts/GenericCollection.cs
+++ Assets/Scripts/GenericCollection.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 public class GenericCollection : MonoBehaviour
 {
     // List
-    void Start()
+    /*void Start()
     {
         List<int> numbers = new List<int>();
         numbers.Add(1);
@@ -22,5 +22,31 @@ public class GenericCollection : MonoBehaviour
         }
 
         Debug.Log(numbers.Count);
+    }*/
+
+    // Dictionary
+    void Start()
+    {
+        Dictionary<string, int> nameAndAges = new Dictionary<string, int>();
+        nameAndAges.Add("철수", 30);
+        nameAndAges.Add("영희", 25);
+        nameAndAges.Add("유리", 35);
+
+        foreach (var tmp in nameAndAges) 
+        { 
+            Debug.Log($"{tmp.Key} {tmp.Value}");
+        } 
+
+        foreach (var key in nameAndAges.Keys) 
+        { 
+            Debug.Log(key); 
+        } 
+
+        foreach (var value in nameAndAges.Values) 
+        { 
+            Debug.Log(value); 
+        } 
+                
+        Debug.Log($"{nameAndAges["영희"]}"); 
     }
 }
