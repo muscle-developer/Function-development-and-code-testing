8ebd7b61ba74636f2c591a86bf6813c0eacb0e4a
muscle-developer <sintyoung5683@naver.com>
2024-04-17 20:50:28 +0900

    C# 메모리할당(값, 참조 형식) 진행중

---
+++ Assets/Scripts/MemoryAllocation.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System;
+
+public class MemoryAllocation : MonoBehaviour
+{
+    int num1 = 10; // 값형식 변수
+    char ch = 'A'; // 값형식 변수
+    
+    string name = "John"; // 참조형식 변수
+    object obj = new object(); // 참조형식 변수
+}
