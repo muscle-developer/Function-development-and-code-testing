3e798b4a1fc0b2d47e9fa81c607c38fc5a71cd26
muscle-developer <sintyoung5683@naver.com>
2024-04-24 21:06:03 +0900

    스크립트수정중

--- Assets/Scripts/MemoryAllocation.cs
+++ Assets/Scripts/MemoryAllocation.cs
@@ -3,29 +3,9 @@ using System;
 
 public class MemoryAllocation : MonoBehaviour
 {
-    private Person person1;
-    private Person person2;
-    private Person person3;
-
     void Start ()
     {
-        person3 = null;  // 널(Null) 값
 
-        // Person 객체 생성 및 초기화
-        person1 = new Person();
-        person1.Name = "엘리스";
-
-        // person1이 참조하는 객체를 person2도 참조
-        person2 = person1;
-
-        // 결과 출력
-        Debug.Log($"person1 Name: {person1.Name}, person2 Name: {person2.Name}");
-
-        // person1의 객체의 속성 변경
-        person1.Name = "존슨";
-
-        // 변경된 결과 출력
-        Debug.Log($"person1 Name: {person1.Name}, person2 Name: {person2.Name}");
     }
 }
 
@@ -50,17 +30,25 @@ class Person
 
 class ProgramReference
 {
-    public static void Main()
+    static void Main()
     {
-        Person person1 = new Person();
-        person1.Name = "엘리스";
+        // // person3 = null;  // 널(Null) 값
 
-        Person person2 = person1; // person1이 참조하는 객체를 person2도 참조
+        // // Person 객체 생성 및 초기화
+        // person1 = new Person();
+        // person1.Name = "엘리스";
 
-        Console.WriteLine($"person1 Name: {person1.Name}, person2 Name: {person2.Name}");
+        // // person1이 참조하는 객체를 person2도 참조
+        // person2 = person1;
 
-        person1.Name = "존슨"; // person1의 객체의 속성 변경
-        Console.WriteLine($"person1 Name: {person1.Name}, person2 Name: {person2.Name}");
+        // // 결과 출력
+        // Debug.Log($"person1 Name: {person1.Name}, person2 Name: {person2.Name}");
+
+        // // person1의 객체의 속성 변경
+        // person1.Name = "존슨";
+
+        // // 변경된 결과 출력
+        // Debug.Log($"person1 Name: {person1.Name}, person2 Name: {person2.Name}");
     }
 }
 
