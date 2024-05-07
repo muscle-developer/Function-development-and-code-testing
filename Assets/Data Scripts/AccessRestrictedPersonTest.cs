0125804a609efb1d764ceb251cecae981eb14406
muscle-developer <sintyoung5683@naver.com>
2024-05-07 20:30:00 +0900

    C# 메서드에 관한 코드 - 메서드의 차이

--- Assets/Data Scripts/AccessRestrictedPersonTest.cs
+++ Assets/Data Scripts/AccessRestrictedPersonTest.cs
@@ -2,8 +2,21 @@ using UnityEngine;
 
 public class AccessRestrictedPersonTest : MonoBehaviour
 {
+
     void Start()
     {
-        
+        Program();
+        NumberProgram();
+    }
+
+    public void Program()
+    {
+        Debug.Log("프로그램 실행을 위한 메서드 : Return이 필요없음!!");
+    }
+
+    public int NumberProgram()
+    {   
+        Debug.Log("프로그램 실행을 위한 메서드 : Return이 필요하다!!");
+        return 0;    
     }
 }
