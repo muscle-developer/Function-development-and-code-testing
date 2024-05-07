23f949237fc33dbcd4f39a716d213b84bc43285e
muscle-developer <sintyoung5683@naver.com>
2024-05-07 20:57:38 +0900

    Bool 응용 함수 설명

--- Assets/Data Scripts/AccessRestrictedPersonTest.cs
+++ Assets/Data Scripts/AccessRestrictedPersonTest.cs
@@ -1,26 +1,34 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AccessRestrictedPersonTest : MonoBehaviour
 {
-    private int a = 1;
-    private int b = 2;
+    private bool isFruit = false;
 
-    void Start()
-    {
-        Program();
-        NumberProgram(b, a);
-    }
-
-    public void Program()
-    { 
-        // Debug.Log("a의 값:" + a + "\n" + "b의 값:" + b);
-        Debug.Log($"a의 값: {a}\n b의 값: {b}");
-    }
-
-    public int NumberProgram(int numberA, int numberB)
+    void Update()
     {   
-        // Debug.Log("Number A의 값:" + numberA + "\n" + "Number B의 값:" + numberB);
-        Debug.Log($"Number A의 값: {numberA}\n Number B의 값: {numberB}");
-        return numberA + numberB;    
+        if (Input.GetKey(KeyCode.Q))
+        {
+            isFruit = true;   
+            Debug.Log("Q 버튼이 눌렸습니다.");
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {   
+            isFruit = false;
+            Debug.Log("W 버튼이 눌렸습니다.");
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+            BoolProgram(isFruit);        
+    }   
+
+    public bool BoolProgram(bool fruit)
+    {
+        if(fruit)
+            Debug.Log("사과");
+        else
+            Debug.Log("사탕");
+
+        return fruit;
     }
 }
