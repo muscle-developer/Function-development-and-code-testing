7e4ce59f30160bd8cb44a497229dbd07dd5f145d
muscle-developer <sintyoung5683@naver.com>
2024-05-10 20:46:57 +0900

    로그 보기위한 코드 수정

--- Assets/Data Scripts/AccessRestrictedPersonTest.cs
+++ Assets/Data Scripts/AccessRestrictedPersonTest.cs
@@ -1,9 +1,30 @@
 using Unity.VisualScripting;
+using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 
 public class AccessRestrictedPersonTest : MonoBehaviour
 {
     private bool isFruit = false;
+
+    private int a = 1;
+    private int b = 2;
+
+    // void Start()
+    // {
+    //     Program();
+    //     NumberProgram(b, a);
+    // }
+
+    // public void Program()
+    // {
+    //     Debug.Log($"a의 값 : {a}\n b의 값 : {b}");
+    // }
+
+    // public int NumberProgram(int numberA, int numberB)
+    // {
+    //     Debug.Log($"numberA의 값 : {numberA}\n numberB의 값 : {numberB}");
+    //     return numberA + numberB;
+    // }
 
     void Update()
     {   
