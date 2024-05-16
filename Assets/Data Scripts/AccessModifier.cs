2aacb33ae23208e796466766b1fe6100f08ebddc
muscle-developer <sintyoung5683@naver.com>
2024-05-16 17:47:06 +0900

    스크립트 수정중

--- Assets/Data Scripts/AccessModifier.cs
+++ Assets/Data Scripts/AccessModifier.cs
@@ -5,13 +5,5 @@ using UnityEngine;
 public class AccessModifier : MonoBehaviour
 {
     // 접근 제한자 종류
-    public void A()
-    {
-        
-    }
-
-    private void B()
-    {
-        
-    }
+    
 }
