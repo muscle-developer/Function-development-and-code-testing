0f6ebe9e789dbcccfc11178beefad18a68408154
muscle-developer <sintyoung5683@naver.com>
2024-05-18 16:24:56 +0900

    public, private 접근 제한자 코드 추가

--- Assets/Data Scripts/AccessModifier.cs
+++ Assets/Data Scripts/AccessModifier.cs
@@ -4,24 +4,51 @@ using UnityEngine;
 
 public class AccessModifier : MonoBehaviour
 {
-    // 접근 제한자 종류
-    public void TestA()
+    public PublicPlayer player;
+    void Awake()
     {
-
+        player = GameObject.FindObjectOfType<PublicPlayer>();
     }
 
-    private void TestB()
-    {
+    void Update()
+    {   
+        if(!player)
+            return;
 
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            // Public 변수 접근 및 변경
+            Debug.Log("Initial Health: " + player.health);
+            player.health = 80;
+            Debug.Log("Updated Health: " + player.health);
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            // Public 메서드 호출
+            player.TakeDamage(30);
+            Debug.Log("Health after damage: " + player.health);
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            // 더 큰 피해를 입히기
+            player.TakeDamage(100);
+            Debug.Log("Health after massive damage: " + player.health);
+        }
     }
+}
 
-    protected void TestC()
-    {
+public class PublicPlayer : MonoBehaviour
+{
+    // Public 변수, 모든 클래스에서 접근 가능
+    public int health = 100;
 
-    }
-    
-    internal void TestD()
+    // Public 메서드, 모든 클래스에서 접근 가능
+    public void TakeDamage(int damage)
     {
-        
+        health -= damage;
+        if (health < 0)
+        {
+            health = 0;
+        }
     }
 }
