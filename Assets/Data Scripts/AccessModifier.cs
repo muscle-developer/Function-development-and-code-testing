b1282197bb5951daca0f5cc9d1a8275b8facece7
muscle-developer <sintyoung5683@naver.com>
2024-05-18 15:50:52 +0900

    접근 제한자 테스트 코드 작성

--- Assets/Data Scripts/AccessModifier.cs
+++ Assets/Data Scripts/AccessModifier.cs
@@ -5,5 +5,23 @@ using UnityEngine;
 public class AccessModifier : MonoBehaviour
 {
     // 접근 제한자 종류
+    public void TestA()
+    {
+
+    }
+
+    private void TestB()
+    {
+
+    }
+
+    protected void TestC()
+    {
+
+    }
     
+    internal void TestD()
+    {
+        
+    }
 }
