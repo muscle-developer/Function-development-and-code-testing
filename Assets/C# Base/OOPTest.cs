10020c0a7b6e17e3fc062dfab67329fd5003935d
muscle-developer <sintyoung5683@naver.com>
2024-05-23 18:49:30 +0900

    객체지향 스크립트 진행중

--- Assets/C# Base/OOPTest.cs
+++ Assets/C# Base/OOPTest.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class OOPTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // 캡슐화
+    // 상속
 }
