2c0ca0bee2f31c25b68e0a6411bec60cb4e495bf
muscle-developer <sintyoung5683@naver.com>
2024-06-24 20:50:36 +0900

    스크립트명 수정 및 코드추가

---
+++ Assets/Data Scripts/ArrayAndListTest.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayAndListTest : MonoBehaviour
+{    
+    
+}
+
+public class ArrayTest : MonoBehaviour
+{
+    
+}
+
+public class ListTest : MonoBehaviour
+{
+    
+}
