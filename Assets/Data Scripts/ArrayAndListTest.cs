6400fba413c39ab1d47afdd388abb6f11a87deca
muscle-developer <sintyoung5683@naver.com>
2024-06-25 17:21:20 +0900

    Array 와 List를 비교한 스크립트 추가

--- Assets/Data Scripts/ArrayAndListTest.cs
+++ Assets/Data Scripts/ArrayAndListTest.cs
@@ -5,15 +5,68 @@ using UnityEngine;
 
 public class ArrayAndListTest : MonoBehaviour
 {    
-    
+    public void Start()
+    {
+        ArrayTest arrayTest = new ArrayTest();
+        arrayTest.Main();
+
+        ListTest listTest = new ListTest();
+        listTest.Main();
+    }
 }
 
-public class ArrayTest : MonoBehaviour
+public class ArrayTest : ArrayAndListTest
 {
-    
+    public void Main()
+    {
+        // 배열 선언과 초기화
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        // 배열 요소 접근
+        Debug.Log("첫 번째 요소: " + numbers[0]);
+
+        // 배열 요소 수정
+        numbers[1] = 10;
+        Debug.Log("수정된 두 번째 요소: " + numbers[1]);
+
+        // 배열의 길이
+        Debug.Log("배열의 길이: " + numbers.Length);
+
+        // 배열의 반복 처리
+        Debug.Log("배열 요소:");
+        foreach (int number in numbers)
+        {
+            Debug.Log(number);
+        }
+    }    
 }
 
-public class ListTest : MonoBehaviour
-{
-    
+public class ListTest : ArrayAndListTest
+{   
+    public void Main()
+    {
+        // 리스트 선언과 초기화
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+        // 리스트 요소 접근
+        Debug.Log("첫 번째 요소: " + numbers[0]);
+
+        // 리스트 요소 수정
+        numbers[1] = 10;
+        Debug.Log("수정된 두 번째 요소: " + numbers[1]);
+
+        // 리스트의 길이
+        Debug.Log("리스트의 길이: " + numbers.Count);
+
+        // 리스트에 요소 추가
+        numbers.Add(6);
+        Debug.Log("추가된 요소: " + numbers[5]);
+
+        // 리스트의 반복 처리
+        Debug.Log("리스트 요소:");
+        foreach (int number in numbers)
+        {
+            Debug.Log(number);
+        }
+    }   
 }
