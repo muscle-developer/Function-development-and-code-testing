a62f8a95a920f842284b2eb465010499305fe4cc
muscle-developer <sintyoung5683@naver.com>
2024-07-05 20:34:31 +0900

    UIPopup 스크립트 완성본 푸시

--- Assets/Function Scripts/UIPopup.cs
+++ Assets/Function Scripts/UIPopup.cs
@@ -5,27 +5,9 @@ using System;
 
 public class UIPopup : MonoBehaviour
 {
-    [SerializeField]
-	protected bool addBackground = true;
-    [SerializeField]
-	protected bool allowCloseByBackgroundClick = true;
-    protected GameObject popupBack = null;
-    protected bool isOpening = false;
-    protected bool isClosing = false;
     public virtual void Open()
     {
         this.gameObject.SetActive(true);
-		
-		isOpening = true;
-
-        // if(addBackground)
-		// {
-		// 	AddBackground();
-
-		// 	var image = popupBack.GetComponent<Image>();
-		// 	if (image != null)
-		// 		image.color = new Color(image.color.r, image.color.g, image.color.b, 150f / 255f);
-		// }
     }
 
     public virtual void Refresh()
@@ -36,43 +18,16 @@ public class UIPopup : MonoBehaviour
 
     public virtual void Close()
 	{
-        Close();
+        ClosePopup();
 	}
 
     // 닫기 기능
-    public virtual void Close(bool foreSkip = false)
+    protected virtual void ClosePopup(bool foreSkip = false)
     {
         // 열리고 있는중이거나, 닫히는중이면 실행하지 않는다.
-        if(isOpening || isClosing || !this.gameObject.activeSelf)
+        if(!this.gameObject.activeSelf)
 			return;
 
-		isClosing = true;
 		this.gameObject.SetActive(false);
-
-        // if(addBackground)
-		// {
-		// 	if(popupBack == null)
-		// 	{
-		// 		AddBackground();
-		// 	}
-			
-		// 	var image = popupBack.GetComponent<Image>();
-		// 	if (image != null)
-		// 		image.color = new Color(image.color.r, image.color.g, image.color.b, 0f);
-		// }
     }
-
-    // protected virtual void AddBackground()
-	// {
-	// 	if(popupBack == null)
-	// 	{
-	// 		popupBack = Instantiate<Transform>(Resources.Load<Transform>("Popup Back")).gameObject;
-	// 		popupBack.transform.SetParent(this.transform.parent, false);
-	// 		popupBack.transform.SetSiblingIndex(transform.GetSiblingIndex());
-	// 		if(allowCloseByBackgroundClick)
-	// 			popupBack.GetComponent<Button>().onClick.AddListener(Close);
-	// 	}
-
-	// 	popupBack.gameObject.SetActive(true);
-    // }
 }
