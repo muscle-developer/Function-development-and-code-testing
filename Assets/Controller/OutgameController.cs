830f61f75e6b36ebe0cb569732cf9900197f06a2
muscle-developer <sintyoung5683@naver.com>
2024-07-08 20:32:21 +0900

    아웃게임 컨트롤러 수정

--- Assets/Controller/OutgameController.cs
+++ Assets/Controller/OutgameController.cs
@@ -6,8 +6,29 @@ public class OutgameController : MonoBehaviour
 {
     public static OutgameController Instance;
 
+    [Header("UI Popup")]
+    [SerializeField]
+    private TestPopup testPopup;
+
     private void Awake()
 	{
-		OutgameController.Instance = this;
+        // 싱글톤 패턴 구현
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); // 씬 전환 시 파괴되지 않도록 설정
+        }
+        else
+        {
+            Destroy(gameObject); // 이미 인스턴스가 존재하면 현재 오브젝트를 파괴
+        }
 	}
+
+    public void OpenTestPopup()
+    {
+        if (testPopup != null)
+            testPopup.gameObject.SetActive(true);
+            
+        testPopup.Open();
+    }
 }
