c04a5c57474dd330de3c7682c9b23fea1481cb52
muscle-developer <sintyoung5683@naver.com>
2024-07-10 20:26:01 +0900

    DontDestroyOnLoad 주의 사항 :  한 번에 하나의 인스턴스 유지로 수정

--- Assets/Utils/DontDestroyOnLoad.cs
+++ Assets/Utils/DontDestroyOnLoad.cs
@@ -2,8 +2,21 @@ using UnityEngine;
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
+    private static DontDestroyOnLoad instance;
+
     private void Awake()
     {
-        DontDestroyOnLoad(this.gameObject);
+        // 중복된 인스턴스가 있는지 확인
+        if (instance == null)
+        {
+            // 인스턴스가 없으면 이 오브젝트를 인스턴스로 설정
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // 이미 인스턴스가 있으면 새로운 오브젝트를 파괴
+            Destroy(gameObject);
+        }
     }
 }
