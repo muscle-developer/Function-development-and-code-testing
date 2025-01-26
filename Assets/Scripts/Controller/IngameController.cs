6b6e76657315c0c65a7b089da3f12a287b6e336f
muscle-developer <sintyoung5683@naver.com>
2025-01-26 19:32:56 +0900

    인게임 컨트롤러 수정

--- Assets/Scripts/Controller/IngameController.cs
+++ Assets/Scripts/Controller/IngameController.cs
@@ -6,9 +6,34 @@ public class IngameController : MonoBehaviour
 {
     public static IngameController Instance;
 
+    [SerializeField]
+    private UIViewPowerSaving uiViewPowerSaving;
 
     void Awake()
     {
         Instance = this;
+    }
+
+    private IEnumerator LogicOnEverySecondCoroutine()
+    {
+        var lastTime = (int)Time.realtimeSinceStartup;
+        while(true)
+        {
+            var currentTime = (int)Time.realtimeSinceStartup;
+
+            if(currentTime - lastTime >= 1)
+            {
+                // 절전모드가 실행중이라면
+                if(uiViewPowerSaving.IsOn)
+                {   
+                    // 인게임 내에서 최소화 할것들 추가
+                    // ex) 그래픽, 조명, 사운드 끄기 등...
+                }
+
+                lastTime = currentTime;
+            }
+
+            yield return null;
+        }
     }
 }
