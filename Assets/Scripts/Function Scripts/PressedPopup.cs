5cbfd6f2558ce2be48a61e6b8e63cfa10081ebfb
muscle-developer <sintyoung5683@naver.com>
2025-02-06 16:32:15 +0900

    버튼 연속눌림 테스트를 위한 팝업 생성

---
+++ Assets/Scripts/Function Scripts/PressedPopup.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.MemoryProfiler;
+using UnityEngine;
+
+public class PressedPopup : UIPopup
+{
+    public override void Open()
+    {
+        base.Open();  
+    }
+
+    public override void Close()
+    {
+        base.Close();
+    }
+}
