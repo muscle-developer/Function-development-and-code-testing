e3dbeb891b5bfa61c62f7e9be7011b0d39585a60
muscle-developer <sintyoung5683@naver.com>
2025-02-06 18:17:36 +0900

    한번의 클릭만 할 때 코드

--- Assets/Scripts/Function Scripts/PressedPopup.cs
+++ Assets/Scripts/Function Scripts/PressedPopup.cs
@@ -1,17 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.MemoryProfiler;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class PressedPopup : UIPopup
 {
+    [SerializeField]
+    private TMP_Text numberText;
+    private int number = 0;
+
     public override void Open()
     {
         base.Open();  
+        Init();
     }
 
     public override void Close()
     {
         base.Close();
+    }
+
+    void Init()
+    {
+        number = 0;
+        numberText.text = number.ToString();
+    }
+
+    public void PlusButtonClicked()
+    {
+        number += 1;
+        numberText.text = number.ToString();
+    }
+    
+    public void MinusButtonClicked()
+    {
+        number -= 1;
+        numberText.text = number.ToString();
+    }
+
+    public void ResetButtonClicked()
+    {
+        Init();
     }
 }
