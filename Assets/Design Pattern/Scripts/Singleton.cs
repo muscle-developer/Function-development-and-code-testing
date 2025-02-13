c00ee0bddf21fac65ba16fb22863a6e17662fc84
muscle-developer <sintyoung5683@naver.com>
2025-02-13 19:46:37 +0900

    싱글톤 스크립트 생성

---
+++ Assets/Design Pattern/Scripts/Singleton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+    // 전역적으로 접근 가능한 유일한 인스턴스
+    private static Singleton instance;
+
+    public static Singleton Instance
+    {
+        get { return instance; }
+        private set { instance = value; }
+    }
+
+
+    private void Awake()
+    {
+        // 이미 인스턴스가 존재하면 새로 생성되는 인스턴스를 제거
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // 현재 인스턴스를 설정
+        instance = this;
+        
+        // 씬이 변경되더라도 유지되도록 설정
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PrintMessage(string message)
+    {
+        Debug.Log(message);
+    }
+}
+
