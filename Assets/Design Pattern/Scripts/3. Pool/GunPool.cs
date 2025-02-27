644ec7b6d9de8ab05c9ef92cfa47b03d6d2eba5a
muscle-developer <sintyoung5683@naver.com>
2025-02-27 20:17:37 +0900

    총 스크립트 생성 - 풀링

---
+++ Assets/Design Pattern/Scripts/3. Pool/GunPool.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class GunPool : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public int poolSize = 10; // 총알 풀 크기
+
+    private void Start()
+    {
+        // 총알 풀 생성
+        PoolManager.Instance.CreatePool("Bullet", bulletPrefab, poolSize);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // 마우스 왼쪽 버튼 클릭
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = PoolManager.Instance.GetObjectFromPool("Bullet");
+
+        if (bullet != null)
+        {
+            bullet.transform.position = firePoint.position;
+            bullet.transform.LookAt(firePoint.position + firePoint.forward);
+        }
+        else
+        {
+            Debug.Log("No available bullets in pool!");
+        }
+    }
+}
