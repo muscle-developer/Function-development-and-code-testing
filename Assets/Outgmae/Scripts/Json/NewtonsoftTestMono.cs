b03c7d64069d80f4e3f31b5c384b6b1c8e4e738f
muscle-developer <sintyoung5683@naver.com>
2025-03-06 17:38:49 +0900

    NewtonSoft Json이 모노비헤이비어 상속받은 클래스를 Json변환할 수 있는지 체크

---
+++ Assets/Outgmae/Scripts/Json/NewtonsoftTestMono.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewtonsoftTestMono : MonoBehaviour
+{
+    public int i = 10;
+    public float f = 5.0f;
+    public bool b = true;
+}
