cabc41a0235870c0c195c5d23b8865c434ae25c7
muscle-developer <sintyoung5683@naver.com>
2025-03-14 19:47:21 +0900

    옵션팝업 스크립트 생성

--- Assets/Outgame/Scripts/Function Scripts/OptionPopup.cs
+++ Assets/Outgame/Scripts/Function Scripts/OptionPopup.cs
@@ -8,12 +8,11 @@ using System.Collections.Generic;
 public class OptionPopup : UIPopup
 {
     [SerializeField]
-	private TMP_Dropdown languageDropDown;
+	private TMP_Dropdown languageDropdown;
 
     public override void Open()
     {
         base.Open();
-        Refresh();
     }
 
     public override void Close()
@@ -21,59 +20,22 @@ public class OptionPopup : UIPopup
         base.Close();
     }
 
-    // public void Refresh()
-    // {
-    //     this.languageDropDown.ClearOptions();
-    //     foreach(var tmp in Enum.GetValues(typeof(OptionManager.LanguageModeType)))
-	// 	{
-	// 		var stringToUse = tmp.ToString();
+     public override void Refresh()
+     {
+        languageDropdown.ClearOptions();
+     }
 
-	// 		if(stringToUse == "zh_cht")
-	// 		{
-	// 			this.languageDropDown.options.Add(new TMP_Dropdown.OptionData("繁體中文"));
-	// 		}
-	// 		else if(stringToUse == "zh_chs")
-	// 		{
-	// 			this.languageDropDown.options.Add(new TMP_Dropdown.OptionData("简体中文"));
-	// 		}
-	// 		else if(stringToUse == "th")
-	// 		{
-	// 			this.languageDropDown.options.Add(new TMP_Dropdown.OptionData("Thai"));
-	// 		}
-	// 		else
-	// 		{
-	// 			var textTouse = new CultureInfo(stringToUse).NativeName;
-	// 			var tmp2 = new TMP_Dropdown.OptionData(textTouse.First().ToString().ToUpperInvariant() + textTouse.Substring(1));
-	// 			this.languageDropDown.options.Add(tmp2);
-	// 		}
-	// 	}
-	// 	this.languageDropDown.value = (int)OptionManager.Instance.Language;
-	// 	this.languageDropDown.RefreshShownValue();
-    // }
+    public void OnLanguageChange(int index)
+    {
+        string selectedLanguage = "";
 
-    // public void OnLangguageDropDown(int id)
-	// {
-	// 	if(this.langugateDropDown.value == (int)OptionManager.Instance.Language)
-	// 		return;
-			
-	// 	UIManager.Instance.ShowAlertLocalized(
-	// 		"message_change_language",
-	// 		() => {
-	// 			OptionManager.Instance.Language = (OptionManager.LanguageModeType)id;
-	// 			PlayerPrefs.SetInt("is_server_lanugage_set", 0);
+        switch (index)
+        {
+            case 0: selectedLanguage = "ko"; break; // 한국어
+            case 1: selectedLanguage = "en"; break; // 영어
+            case 2: selectedLanguage = "ja"; break; // 일본어
+        }
 
-	// 			if(ServerNetworkManager.GetServerType == ServerNetworkManager.ServerType.Korea)
-	// 				OptionManager.Instance.ChatLanguage = OptionManager.ChatLanguageModeType.ko;
-	// 			else
-	// 				OptionManager.Instance.ChatLanguage = OptionManager.ChatLanguageModeType.global;
-
-	// 			ChatManager.Instance.Disconnect();
-
-	// 			SceneChangeManager.Instance.LoadMainGame(true, true);
-	// 		},
-	// 		() => {
-	// 			this.langugateDropDown.value = (int)OptionManager.Instance.Language;
-	// 		}
-	// 	);
-	// }
+        LanguageManager.Instance.LoadLanguage(selectedLanguage);
+    }
 }
