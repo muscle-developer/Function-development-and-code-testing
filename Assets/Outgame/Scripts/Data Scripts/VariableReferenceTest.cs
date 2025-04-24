c5d863736be87b105f1e2904e74c7f37ddff1dd7
muscle-developer <sintyoung5683@naver.com>
2025-04-24 20:16:57 +0900

    out 케이스 추가

--- Assets/Outgame/Scripts/Data Scripts/VariableReferenceTest.cs
+++ Assets/Outgame/Scripts/Data Scripts/VariableReferenceTest.cs
@@ -6,22 +6,26 @@ public class VariableReferenceTest : MonoBehaviour
 {
     void Start()
     {
+        // ref 예시
         int myNumber = 10;
         AddFive(ref myNumber);
         Debug.Log(myNumber); // 출력: 15
-    }
 
+        // out 예시
+        int result;
+        CreateNumber(out result);
+        Debug.Log(result); // 출력: 42
+    }
+    
+    // ref 예시 함수
     public void AddFive(ref int number)
     {
         number += 5;
     }
-    
-    // 테스트 커밋
-//     int testA;
-// TestRef(ref testA); // testA 컴파일 에러 발생.
 
-// void TestRef(ref int a)
-// {
-//    a = 10;
-// }
+    // out 예시 함수
+    void CreateNumber(out int number)
+    {
+        number = 42; // 반드시 값을 할당해야 함
+    }
 }
