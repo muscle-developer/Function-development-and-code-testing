8856c5c9c82932f40feaf318845e3e1f25c639ba
muscle-developer <sintyoung5683@naver.com>
2025-05-08 18:47:46 +0900

    cs수정중

--- Assets/Scenes/Scrips/Asynchronous/UniTaskTest.cs
+++ Assets/Scenes/Scrips/Asynchronous/UniTaskTest.cs
@@ -1,18 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
+
 
 public class UniTaskTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
