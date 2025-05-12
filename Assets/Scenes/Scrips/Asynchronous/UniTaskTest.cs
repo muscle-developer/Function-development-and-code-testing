40d9fa1772f32a973eacf6bc4baaa854fae27b47
muscle-developer <sintyoung5683@naver.com>
2025-05-12 20:07:57 +0900

    코루틴과 유니테스크 시간대기 비교 코드 추가

--- Assets/Scenes/Scrips/Asynchronous/UniTaskTest.cs
+++ Assets/Scenes/Scrips/Asynchronous/UniTaskTest.cs
@@ -1,10 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
+using System;
 using System.Threading.Tasks;
-
 
 public class UniTaskTest : MonoBehaviour
 {
+    public GameObject coroutineObj;
+    public GameObject uniTaskObj;
 
+    void Start()
+    {
+        StartCoroutine(Wait1Second());
+        Wait1SecondAsync().Forget();
+    }
+
+    private IEnumerator Wait1Second()
+    {
+        yield return new WaitForSeconds(3f);
+        coroutineObj.gameObject.SetActive(false);
+        Debug.Log("코루틴 오브젝트 3초뒤 꺼짐");
+    }
+
+    public async UniTask Wait1SecondAsync()
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(3));
+        uniTaskObj.SetActive(false);
+        Debug.Log("유니테스크 오브젝트 3초뒤 꺼짐");
+    }
 }
